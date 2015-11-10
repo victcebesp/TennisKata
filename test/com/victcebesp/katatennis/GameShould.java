dd7f1e18761b0999c9a87ebbdcba603fb9899cb6
Victor Ceballos <victcebesp@gmail.com>
2015-11-10 16:14:40 +0000

    Strings (playerA and playerB) changed to objects with a String playerName field

--- test/com/victcebesp/katatennis/GameShould.java
+++ test/com/victcebesp/katatennis/GameShould.java
@@ -8,8 +8,8 @@ import static org.hamcrest.MatcherAssert.assertThat;
 
 public class GameShould {
 
-    public final String PLAYER_A = "playerA";
-    public final String PLAYER_B = "playerB";
+    Player playerA = new Player("playerA");
+    Player playerB = new Player("playerB");
     private Game game;
 
     @Before
@@ -18,38 +18,53 @@ public class GameShould {
     }
 
     @Test public void
-    return_true_when_there_is_a_set_winner() {
-        addXPointsTo(PLAYER_A, 3);
-
-        assertThat(game.checkIfThereIsASetWinner(), is(true));
-    }
-
-    @Test public void
-    return_player_two_as_the_winner_when_it_has_two_advantages_more_than_playerA() {
-        addXPointsTo(PLAYER_A, 3);
-        addXPointsTo(PLAYER_B, 5);
-
-        assertThat(game.decideWhichPlayerWinTheSet(), is("playerB"));
-    }
-
-    @Test public void
     return_each_player_score() {
-        addXPointsTo(PLAYER_A, 2);
-        addXPointsTo(PLAYER_B, 1);
+        addXPointsTo(playerA.getPlayerName(), 2);
+        addXPointsTo(playerB.getPlayerName(), 1);
 
-        assertThat(game.carryTheGame(), is("Player A score: 30, Player B score: 15"));
+        assertThat(game.printGameStatus(), is("Player A score: 30, Player B score: 15"));
     }
+
     @Test public void
     return_that_both_players_have_the_same_score() {
-        addXPointsTo(PLAYER_A, 2);
-        addXPointsTo(PLAYER_B, 2);
+        addXPointsTo(playerA.getPlayerName(), 2);
+        addXPointsTo(playerB.getPlayerName(), 2);
 
-        assertThat(game.carryTheGame(), is("30 All"));
+        assertThat(game.printGameStatus(), is("30 All"));
     }
+
+    @Test public void
+    return_deuce_when_both_players_have_as_score_40() {
+        addXPointsTo(playerA.getPlayerName(), 3);
+        addXPointsTo(playerB.getPlayerName(), 3);
+
+        assertThat(game.printGameStatus(), is("Deuce"));
+    }
+
+    @Test public void
+    return_playerA_has_an_advantage_when_deuce_happen_and_playerA_scores() {
+        addXPointsTo(playerA.getPlayerName(), 4);
+        addXPointsTo(playerB.getPlayerName(), 3);
+
+        assertThat(game.printGameStatus(), is("playerA has an advantage"));
+    }
+
+
     private void addXPointsTo(String player, int times) {
         for (int i = 0; i < times; i++) {
             game.addPointsToPlayer(player);
         }
     }
 
+}
+class Player {
+    private String playerName;
+
+    public Player(String playerName) {
+        this.playerName = playerName;
+    }
+
+    public String getPlayerName() {
+        return playerName;
+    }
 }
