ccb1ec52c990f7bc5fd8c7d61c18c49429cafaef
Victor Ceballos <victcebesp@gmail.com>
2015-11-10 16:33:30 +0000

    Refactor: changed addXPointsTo parameter from String to object

--- test/com/victcebesp/katatennis/GameShould.java
+++ test/com/victcebesp/katatennis/GameShould.java
@@ -19,44 +19,45 @@ public class GameShould {
 
     @Test public void
     return_each_player_score() {
-        addXPointsTo(playerA.getPlayerName(), 2);
-        addXPointsTo(playerB.getPlayerName(), 1);
+        addXPointsTo(playerA, 2);
+        addXPointsTo(playerB, 1);
 
         assertThat(game.printGameStatus(), is("Player A score: 30, Player B score: 15"));
     }
 
     @Test public void
     return_that_both_players_have_the_same_score() {
-        addXPointsTo(playerA.getPlayerName(), 2);
-        addXPointsTo(playerB.getPlayerName(), 2);
+        addXPointsTo(playerA, 2);
+        addXPointsTo(playerB, 2);
 
         assertThat(game.printGameStatus(), is("30 All"));
     }
 
     @Test public void
     return_deuce_when_both_players_have_as_score_40() {
-        addXPointsTo(playerA.getPlayerName(), 3);
-        addXPointsTo(playerB.getPlayerName(), 3);
+        addXPointsTo(playerA, 3);
+        addXPointsTo(playerB, 3);
 
         assertThat(game.printGameStatus(), is("Deuce"));
     }
 
     @Test public void
     return_playerA_has_an_advantage_when_deuce_happen_and_playerA_scores() {
-        addXPointsTo(playerA.getPlayerName(), 4);
-        addXPointsTo(playerB.getPlayerName(), 3);
+        addXPointsTo(playerA, 4);
+        addXPointsTo(playerB, 3);
 
         assertThat(game.printGameStatus(), is("playerA has an advantage"));
     }
 
 
-    private void addXPointsTo(String player, int times) {
+    private void addXPointsTo(Player player, int times) {
         for (int i = 0; i < times; i++) {
-            game.addPointsToPlayer(player);
+            game.addPointsToPlayer(player.getPlayerName());
         }
     }
 
 }
+
 class Player {
     private String playerName;
 
