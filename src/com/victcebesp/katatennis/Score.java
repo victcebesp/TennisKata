ac6c3cfe5bbe0d6338020d66208e6e702e0650f4
Victor Ceballos <victcebesp@gmail.com>
2015-11-10 18:25:59 +0000

    Cleanup unused field

--- src/com/victcebesp/katatennis/Score.java
+++ src/com/victcebesp/katatennis/Score.java
@@ -4,12 +4,10 @@ package com.victcebesp.katatennis;
 public class Score {
 
     private int score;
-    private int matchPoints;
     private int advantage;
 
     public Score() {
         this.score = 0;
-        this.matchPoints = 0;
     }
 
     public void addPoint() {
