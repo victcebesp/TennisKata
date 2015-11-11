67719f344138e93e1b727c33d6c03da7057e5416
Victor Ceballos <victcebesp@gmail.com>
2015-11-11 22:58:52 +0000

    Refactor parameter type

--- test/com/victcebesp/katatennis/GameShould.java
+++ test/com/victcebesp/katatennis/GameShould.java
@@ -19,40 +19,40 @@ public class GameShould {
 
     @Test public void
     return_each_player_score() {
-        add(2).pointsToAdd(playerA.getScore());
-        add(1).pointsToAdd(playerB.getScore());
+        add(2).pointsToAdd(playerA);
+        add(1).pointsToAdd(playerB);
 
         assertThat(game.printGameStatus(), is("Player A score: 30, Player B score: 15"));
     }
 
     @Test public void
     return_that_both_players_have_the_same_score() {
-        add(2).pointsToAdd(playerA.getScore());
-        add(2).pointsToAdd(playerB.getScore());
+        add(2).pointsToAdd(playerA);
+        add(2).pointsToAdd(playerB);
 
         assertThat(game.printGameStatus(), is("30 All"));
     }
 
     @Test public void
     return_deuce_when_both_players_have_as_score_40() {
-        add(3).pointsToAdd(playerA.getScore());
-        add(3).pointsToAdd(playerB.getScore());
+        add(3).pointsToAdd(playerA);
+        add(3).pointsToAdd(playerB);
 
         assertThat(game.printGameStatus(), is("Deuce"));
     }
 
     @Test public void
     return_playerA_has_an_advantage_when_deuce_happen_and_playerA_scores() {
-        add(4).pointsToAdd(playerA.getScore());
-        add(3).pointsToAdd(playerB.getScore());
+        add(4).pointsToAdd(playerA);
+        add(3).pointsToAdd(playerB);
 
         assertThat(game.printGameStatus(), is("playerA has an advantage"));
     }
 
     @Test public void
     return_playerB_has_an_advantage_when_deuce_happen_and_playerB_scores() {
-        add(3).pointsToAdd(playerA.getScore());
-        add(4).pointsToAdd(playerB.getScore());
+        add(3).pointsToAdd(playerA);
+        add(4).pointsToAdd(playerB);
 
         assertThat(game.printGameStatus(), is("playerB has an advantage"));
     }
@@ -61,9 +61,17 @@ public class GameShould {
         return new ScoreAdder(points);
     }
 
+    class ScoreAdder {
+        final private int pointsToAdd;
 
+        public ScoreAdder(int points) {
+            this.pointsToAdd = points;
         }
 
+        public void pointsToAdd(Player player) {
+            for (int i = 0; i < pointsToAdd; i++) {
+                player.addPoint();
+            }
         }
     }
 }
